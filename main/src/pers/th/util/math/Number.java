feed30ef1643c5f9dba65ba9a6fe316de24e6b60
lyrieek <tianhao7@foxmail.com>
2017-12-22 19:26:02 +0800

    add Number

---
+++ main/src/pers/th/util/math/Number.java
@@ -0,0 +1,46 @@
+package pers.th.util.math;
+
+public class Number {
+
+	public static StringBuffer analysis(String a, String b) {
+		char[] as = a.toCharArray();
+		char[] bs = b.toCharArray();
+		int lastBsIndex = bs.length - 1;
+		StringBuffer result = new StringBuffer();
+		int storage = 0;
+		for (int i = as.length - 1; i > -1; i--) {
+			// System.out.println(as[i]);
+			int endInt = 0;
+			if (lastBsIndex > -1) {
+				endInt = charToInt(bs[lastBsIndex]);
+			}
+			int appendNumber = charToInt(as[i]) + endInt + storage;
+			if (appendNumber >= 10) {
+				storage = (appendNumber / 10);
+				appendNumber %= 10;
+			} else {
+				storage = 0;
+			}
+			result.append(appendNumber);
+			lastBsIndex--;
+		}
+		for (; lastBsIndex > -1; lastBsIndex--) {
+			int appendNumber = charToInt(bs[lastBsIndex]) + storage;
+			if (appendNumber >= 10) {
+				storage = (appendNumber / 10);
+				appendNumber %= 10;
+			} else {
+				storage = 0;
+			}
+			result.append(appendNumber);
+		}
+		if (storage != 0) {
+			result.append(storage);
+		}
+		return result.reverse();
+	}
+
+	private static int charToInt(char in) {
+		return Integer.parseInt(in + "");
+	}
+}

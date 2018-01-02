aca989e6b4b574680c373ba15f81cf2b6a49d412
lyrieek <tianhao7@foxmail.com>
2018-01-02 19:20:23 +0800

    remove test

--- main/src/pers/th/util/io/XFile.java
+++ main/src/pers/th/util/io/XFile.java
@@ -21,17 +21,14 @@ public class XFile extends File{
 	
 	public static boolean rm(File file) {
 		if (file.isFile()) {
-			System.out.println(1);
 			return file.delete();
 		}
 		for (File item : file.listFiles()) {
 			if (item.isDirectory() && !rm(item)) {
-				System.out.println(2);
 				return false;
 			}
 			System.out.println(item);
 			if (!item.delete()) {
-				System.out.println(12);
 				return false;
 			}
 		}

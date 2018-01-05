7bbff03b7066371b20df12ce5d789fecef4c5043
lyrieek <tianhao7@foxmail.com>
2018-01-05 11:53:45 +0800

    add number test

--- main/src/pers/th/util/math/Number.java
+++ main/src/pers/th/util/math/Number.java
@@ -2,7 +2,26 @@ package pers.th.util.math;
 
 public class Number {
 
-	public static StringBuffer analysis(String a, String b) {
+	public static void main(String[] args) {
+		long s = System.currentTimeMillis();
+		int sum = 0;
+//		String sum = "";
+		for (int i = 0; i < 1000000; i++) {
+//			sum = analysis("22222222", "191991919");
+			sum = Integer.parseInt("22222222") + Integer.parseInt("191991919");//214214141
+		}
+		System.out.println(System.currentTimeMillis() - s);
+		System.out.println(sum);
+	}
+	
+	/**
+	 * 
+	 * 可以计算超long数据,计算小数值速度比普通的计算方式耗时多出三倍
+	 * @param a 
+	 * @param b
+	 * @return
+	 */
+	public static String add(String a, String b) {
 		char[] as = a.toCharArray();
 		char[] bs = b.toCharArray();
 		int lastBsIndex = bs.length - 1;
@@ -24,8 +43,8 @@ public class Number {
 			result.append(appendNumber);
 			lastBsIndex--;
 		}
-		for (; lastBsIndex > -1; lastBsIndex--) {
-			int appendNumber = charToInt(bs[lastBsIndex]) + storage;
+		for (int appendNumber; lastBsIndex > -1; lastBsIndex--) {
+			appendNumber = charToInt(bs[lastBsIndex]) + storage;
 			if (appendNumber >= 10) {
 				storage = (appendNumber / 10);
 				appendNumber %= 10;
@@ -37,7 +56,7 @@ public class Number {
 		if (storage != 0) {
 			result.append(storage);
 		}
-		return result.reverse();
+		return result.reverse().toString();
 	}
 
 	private static int charToInt(char in) {
